1b39261aaab72ed9e5b3ea9fc4e7bc9a6358126a
Kamarool Karim <kamaroolkarim@gmail.com>
2018-03-18 11:34:51 +0800

    initialize grid

--- src/app/_shared/components/navbar/navbar.component.ts
+++ src/app/_shared/components/navbar/navbar.component.ts
@@ -2,24 +2,7 @@ import { Component } from '@angular/core'
 
 @Component({
     selector: 'v-navbar',
-    template: `
-        <ul>
-            <li><a routerLinkActive="active" routerLink="/" [routerLinkActiveOptions]="{ exact: true }">Landing</a></li>
-            <li><a routerLinkActive="active" routerLink="/about">About</a></li>
-            <li><a routerLinkActive="active" routerLink="/contact-developer">Contact Developer</a></li>
-            <li><a routerLinkActive="active" routerLink="/donation">Donation</a></li>
-            <li><a routerLinkActive="active" routerLink="/faq">Faq</a></li>
-            <li><a routerLinkActive="active" routerLink="/hadith">Hadith</a></li>
-        </ul>
-
-        <router-outlet></router-outlet>
-    `,
-    styles: [ `
-        .active {
-            color: green;
-        }
-    `]
+    templateUrl: 'navbar.component.html',
+    styleUrls: ['navbar.component.scss']
 })
-export class NavbarComponent {
-
-}
+export class NavbarComponent {}
