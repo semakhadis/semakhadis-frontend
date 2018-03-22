86c7b0ec0e92bce927fc5fccbb228df292578770
Kamarool Karim <kamaroolkarim@gmail.com>
2018-03-22 09:37:19 +0800

    add navbar-base if route !== index

--- src/app/_shared/components/navbar/navbar.component.ts
+++ src/app/_shared/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core'
+import { Router, NavigationEnd } from '@angular/router'
 
 @Component({
     selector: 'v-navbar',
@@ -7,8 +8,28 @@ import { Component } from '@angular/core'
 })
 export class NavbarComponent {
     hamburgerIsActive: boolean = false
+    currentRoute: string
+
+    /* constructor(private _router: Router) {
+        _router.events.subscribe((url: any) => {
+            this.currentRoute = _router.url
+        })
+    } */
+
+    constructor(private router: Router) {
+        this.router.events.subscribe(event => {
+            if (event instanceof NavigationEnd) {
+                this.isResolving = false
+                this.currentRoute = router.url
+            }
+        })
+    }
 
     toggleHamburger() {
         this.hamburgerIsActive = !this.hamburgerIsActive
+    }
+
+    isIndexRoute() {
+        return this.currentRoute === '/'
     }
 }
