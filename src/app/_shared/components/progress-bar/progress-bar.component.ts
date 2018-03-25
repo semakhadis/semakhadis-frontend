ceba456954f9895a5bca2372f0500a565b2419a9
Kamarool Karim <kamaroolkarim@gmail.com>
2018-03-26 01:35:08 +0800

    update progress bar color

--- src/app/_shared/components/progress-bar/progress-bar.component.ts
+++ src/app/_shared/components/progress-bar/progress-bar.component.ts
@@ -3,8 +3,7 @@ import { Component } from '@angular/core'
 @Component({
     selector: 'v-progress-bar',
     template: `
-        <ng-progress [showSpinner]="false" [color]="'#2eb8ab'" [thick]="true" [ease]="'linear'"></ng-progress>
+        <ng-progress [showSpinner]="false" [color]="'#f4e8b8'" [thick]="true" [ease]="'linear'"></ng-progress>
     `
 })
-export class ProgressBarComponent {
-}
+export class ProgressBarComponent {}
