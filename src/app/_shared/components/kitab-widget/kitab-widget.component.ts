6122217f3ccf3a65493f51410457b952040f826a
Kamarool Karim <kamaroolkarim@gmail.com>
2018-04-10 17:38:59 +0800

    resolve relative path issues in scss

--- src/app/_shared/components/kitab-widget/kitab-widget.component.ts
+++ src/app/_shared/components/kitab-widget/kitab-widget.component.ts
@@ -12,74 +12,74 @@ export class KitabWidgetComponent implements OnInit {
         kitabs: [
             {
                 name: 'Al-Manar Al-Munif',
-                image: '../../assets/images/kitab.jpg',
+                image: '/assets/images/kitab.jpg',
                 writer: 'Penulis 1'
             },
             {
                 name: 'Al-Durr Al-Thamin',
-                image: '../../assets/images/kitab.jpg',
+                image: '/assets/images/kitab.jpg',
                 writer: 'Penulis 2'
             },
             {
                 // tslint:disable-next-line:quotemark
                 name: "Silsilah Al-Ahadith Al-Da'ifah",
-                image: '../../assets/images/kitab.jpg',
+                image: '/assets/images/kitab.jpg',
                 writer: 'Penulis 3'
             },
             {
                 // tslint:disable-next-line:quotemark
                 name: "Tanzih Al-Shari'Ah",
-                image: '../../assets/images/kitab.jpg',
+                image: '/assets/images/kitab.jpg',
                 writer: 'Penulis 4'
             },
             {
                 name: '100 Hadis Lemah & Palsu',
-                image: '../../assets/images/kitab.jpg',
+                image: '/assets/images/kitab.jpg',
                 writer: 'Penulis 5'
             },
             {
                 name: 'Hadis 40 Imam Nawawi',
-                image: '../../assets/images/kitab.jpg',
+                image: '/assets/images/kitab.jpg',
                 writer: 'Penulis 6'
             },
             {
                 name: 'Hadis Bukhari',
-                image: '../../assets/images/kitab.jpg',
+                image: '/assets/images/kitab.jpg',
                 writer: 'Penulis 7'
             },
             {
                 name: 'Al-Umm',
-                image: '../../assets/images/kitab.jpg',
+                image: '/assets/images/kitab.jpg',
                 writer: 'Penulis 8'
             },
             {
                 name: 'Al-Abatil Wa al-Manakir Wa al-Sihah Wa al-Masyahir',
-                image: '../../assets/images/kitab.jpg',
+                image: '/assets/images/kitab.jpg',
                 writer: 'Penulis 9'
             },
             {
                 name: 'Al-Mawdhu’at Min al-Ahadith al-Marfu’at',
-                image: '../../assets/images/kitab.jpg',
+                image: '/assets/images/kitab.jpg',
                 writer: 'Penulis 10'
             },
             {
                 name: 'Al-Qawl al-Musaddad',
-                image: '../../assets/images/kitab.jpg',
+                image: '/assets/images/kitab.jpg',
                 writer: 'Penulis 11'
             },
             {
                 name: 'Dzayl Al-Qawl al-Musaddad',
-                image: '../../assets/images/kitab.jpg',
+                image: '/assets/images/kitab.jpg',
                 writer: 'Penulis 12'
             },
             {
                 name: 'Tartib al-Mawdhu’at',
-                image: '../../assets/images/kitab.jpg',
+                image: '/assets/images/kitab.jpg',
                 writer: 'Penulis 13'
             },
             {
                 name: 'Al-La’ali’ al-Masnu’ah Fi al-Ahadith al-Mawdhu’ah',
-                image: '../../assets/images/kitab.jpg',
+                image: '/assets/images/kitab.jpg',
                 writer: 'Penulis 14'
             }
         ]
